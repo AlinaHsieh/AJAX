756eee100b3da5b74a272281e78242196a14d465
Alina Hsieh <ahsieh74@gmail.com>
2023-07-24 21:09:23 +0800

    使用回傳JSON格式的方式讓前端處理資料

--- app2/api/all.php
+++ app2/api/all.php
@@ -1,8 +1,9 @@
 <?php
-
+header('Content-Type:application/json; charset=utf8');
 $pdo=new PDO('mysql:host=localhost;charset=utf8;dbname=ajax_student','root','');
 $students = $pdo->query("select * from students")->fetchAll();
-foreach($students as $student){
-    echo $student['name'] . '-' . $student['birthday'];
-    echo "<br>";
-}
+// foreach($students as $student){
+//     echo $student['name'] . '-' . $student['birthday'];
+//     echo "<br>";
+// }
+echo json_encode($students); //編寫成json字串
