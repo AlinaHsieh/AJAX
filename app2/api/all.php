7633d14be29d1c30c02922dbcb27551b3976b096
Alina Hsieh <ahsieh74@gmail.com>
2023-07-24 20:45:18 +0800

    使用AJAX取得班級學生資料並呈現在畫面上

--- app2/api/all.php
+++ app2/api/all.php
@@ -1,8 +1,7 @@
 <?php
-//處理查詢資料的請求
+
 $pdo=new PDO('mysql:host=localhost;charset=utf8;dbname=ajax_student','root','');
-$code = $_GET['classroom'];
-$students = $pdo->query("select * from students where classroom = $code")->fetchAll();
+$students = $pdo->query("select * from students")->fetchAll();
 foreach($students as $student){
     echo $student['name'] . '-' . $student['birthday'];
     echo "<br>";
---
+++ app2/api/classes.php
@@ -0,0 +1,12 @@
+<?php
+header("Access-Control-Allow-Origin: *"); //請求CORS
+$pdo=new PDO('mysql:host=localhost;charset=utf8;dbname=ajax_student','root','');
+$classes = $pdo->query("SELECT classroom FROM `students` group by classroom")->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($classes as $class){
+    echo "<button class='btn btn-info m-3' onclick='query({$class['classroom']})'>";
+    //資料撈出後，會去執行query(101、102、103)班級 F12 Element
+    echo $class['classroom'];
+    echo "</button>";
+
+}
